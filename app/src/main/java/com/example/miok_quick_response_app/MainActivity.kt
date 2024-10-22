9964752672759a7653608f0c07f631b0d3c430f3
Harin Ramji <harinharish2002@gmail.com>
2024-10-22 15:25:22 +1300

    correct gradles

--- app/src/main/java/com/example/miok_quick_response_app/MainActivity.kt
+++ app/src/main/java/com/example/miok_quick_response_app/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.miok_quick_response_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.recyclerapp1.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
