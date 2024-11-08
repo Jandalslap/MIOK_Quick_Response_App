768229acf01baeadb1209c443231bd0d35f62e56
Harin Ramji <harinharish2002@gmail.com>
2024-11-08 16:54:02 +1300

    testing

--- app/src/main/java/com/example/miok_quick_response_app/HomeFragment.kt
+++ app/src/main/java/com/example/miok_quick_response_app/HomeFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.miok_quick_response_app.R
+import com.example.miok_quick_response_app.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null // Binding object for accessing views
+    private val binding get() = _binding!! // Safe access to the binding object
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +20,5 @@ class HomeFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 }
