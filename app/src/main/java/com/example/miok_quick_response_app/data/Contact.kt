0a1ce9f02e1367d07826df8f9c331c41b30fd7d2
Harin Ramji <harinharish2002@gmail.com>
2024-11-12 08:37:02 +1300

    Contacts upgrade
    have added a few more upgrades to the contacts

--- app/src/main/java/com/example/miok_quick_response_app/data/Contact.kt
+++ app/src/main/java/com/example/miok_quick_response_app/data/Contact.kt
@@ -1,3 +1,32 @@
-package com.example.miok_quick_response_app.data
+import android.os.Parcel
+import android.os.Parcelable
 
-data class Contact(val name: String)
+data class Contact(
+    val name: String,
+    val dob: String // Date of Birth as String (or use a Date object if needed)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(dob)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object {
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<Contact> {
+            override fun createFromParcel(parcel: Parcel): Contact {
+                return Contact(parcel)
+            }
+
+            override fun newArray(size: Int): Array<Contact?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+}
